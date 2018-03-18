295b4e505c88c62662a1b9eae24da515d5cf17ca
Lachezar <lachezarkostov@gmail.com>
2018-03-18 21:00:41 +0000

    Add map with markers and pagination

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
   	@companies = Company.all
+  	@companies_paginated = Company.paginate(page: params[:page], per_page: 20)
+  	render :layout => 'map'
   end
 end
