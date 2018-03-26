4b6f9f0199bcf98f1f0a520664351af429f12387
Lachezar Kostov <lachezarkostov@gmail.com>
2018-03-26 19:31:15 +0100

    Add remember me and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180325173650) do
+ActiveRecord::Schema.define(version: 20180326160129) do
 
   create_table "companies", force: :cascade do |t|
     t.integer "rank"
@@ -148,6 +148,7 @@ ActiveRecord::Schema.define(version: 20180325173650) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
