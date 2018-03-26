69b8bfe40328f6cfed0f225003be6d8534918ce1
Lachezar Kostov <lachezarkostov@gmail.com>
2018-03-26 10:16:28 +0100

    Create user login logout and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,12 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :first_name, presence: true, length: { maximum: 255 }
   validates :last_name, presence: true, length: { maximum: 255 }
-  validates :password, presence: true, length: { minimum: 6 }
+  
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
